cc906c5e23be3aa38f9f4404be124861bd05ee5f
Timofey Lyadinsky <tima.led12@gmail.com>
2022-02-19 21:40:51 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,26 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let begin = 0, end = 9;
+    let point = "";
+    let tmp = end;
+    let rez = "";
+    while(end <= expr.length){
+        let i = end;
+        while(i > begin){
+            if(expr[i]+expr[i-1] == "01") point="."+point;
+            if(expr[i]+expr[i-1] == "11") point="-"+point;
+            if(expr[i]+expr[i-1] == "**") {rez+=" "; break;}
+            i-=2;
+        }
+        for(let key in MORSE_TABLE){
+            if (key == point) {rez+=MORSE_TABLE[key]; break;}
+        }
+        begin += 10;
+        end+=10;
+        point = "";
+    }
+    return rez;
 }
 
 module.exports = {
